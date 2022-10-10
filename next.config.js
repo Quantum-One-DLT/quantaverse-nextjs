0e98fe265aaa99da9f0cca38b015e8207c073dc8
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-10 09:34:40 -0400

    remove next images fx tst

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 const withImages = require('next-images')
-module.exports = withImages({
+module.exports = {
   assetPrefix: 'https://example.com',
   dynamicAssetPrefix: true,
   inlineImageLimit: 16384,
@@ -8,6 +8,6 @@ module.exports = withImages({
   webpack(config, options) {
     return config
   }
-})
+};
 
    
