6670d2b788805fcc67eeeb6fc53ed3e53a49de79
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-10 02:09:56 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+// next.config.js
 const withImages = require('next-images')
-module.exports = {
-   poweredByHeader: false
-} withImages();
+module.exports = withImages({
+  assetPrefix: 'https://example.com',
+  dynamicAssetPrefix: true,
+  inlineImageLimit: 16384,
+  webpack(config, options) {
+    return config
+  }
+})
+
+   
