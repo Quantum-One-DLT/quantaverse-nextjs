459a44b37380c226733655cc65650e9a20fc4395
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-10 02:26:30 -0400

    Update contentData.js

--- utils/contentData.js
+++ utils/contentData.js
@@ -3,7 +3,7 @@ const contentData = [
     title: 'Configure Other Identity Providers',
     link: 'https://auth0.com/docs/identityproviders',
     description:
-      'Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server.'
+      'Auth0 supports social providers as Facebook, Twitter, and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server.'
   },
   {
     title: 'Enable Multifactor Authentication',
