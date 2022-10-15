4f17fd9f2df60c3984936ef7649dc61864e349df
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-15 14:04:05 -0400

    nextjs config merge

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-=======
 // next.config.js
-const withImages = require('next-images')
 module.exports = {
   assetPrefix: 'https://example.com',
   dynamicAssetPrefix: true,
   inlineImageLimit: 16384,
   poweredByHeader: false,
   webpack(config, options) {
-    return config
->>>>>>> 78d46d6fa814af1704b25950fff48b21aa76e56f
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
   }
 };
 
