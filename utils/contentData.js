38cb42d947f2d363e487dc922628066992b8b1d0
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-15 14:19:28 -0400

    data update

--- utils/contentData.js
+++ utils/contentData.js
@@ -1,27 +1,23 @@
 const contentData = [
   {
-    title: 'Configure Other Identity Providers',
-    link: 'https://auth0.com/docs/identityproviders',
-    description:
-      'Auth0 supports social providers as Facebook, Twitter, and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server.'
+    title: 'Learn more in our whitepaper',
+    link: 'https://www.notion.so/quantumone/Quantum-One-Whitepaper-7df3771e24184020b3ffbf808d140ae6',
+    description: 'Eliminating blockchain investment risk with a solution to simulatenously extract up to $400B of value annually in the US banking sector alone.'
   },
   {
-    title: 'Enable Multifactor Authentication',
-    link: 'https://auth0.com/docs/mfa',
-    description:
-      'Add an extra layer of security by enabling Multi-factor Authentication, requiring your users to provide more than one piece of identifying information. Push notifications, authenticator apps, SMS, and DUO Security are supported.'
+    title: 'Airdrop',
+    link: 'https://etherscan.io/token/0xeaad65885fea47a3b1258935f4ce83aab06fdd3a',
+    description:'The first 3500 users who sign up and verify their identity will automatically receive our testnet ERC20 token symbol "DA-FI" via aidrop. Check out the token profile at Etherscan.io for info related to total supply and transaction history.'
   },
   {
-    title: 'Attack Protection',
-    link: 'https://auth0.com/docs/attack-protection',
-    description:
-      'Auth0 can detect anomalies and stop malicious attempts to access your application. Attack protection can alert you and your users of suspicious activity, as well as block further login attempts.'
+    title: 'Quantum One Stability Fund - Uniswap Liquidity Pool',
+    link: 'https://app.uniswap.org/#/add/v2/ETH/0xeaad65885fea47a3b1258935f4ce83aab06fdd3a',
+    description:'Get in on the ground floor of our stability fund and Uniswap liquidity pool! Keep in mind, DA-FI holders must verify their identity to receive our mainnet ecosystem coin - Quantum One Roton.'
   },
   {
-    title: 'Learn About Rules',
-    link: 'https://auth0.com/docs/rules',
-    description:
-      "Rules are JavaScript functions that execute when a user authenticates to your application. They run once the authentication process is complete, and you can use them to customize and extend Auth0's capabilities."
+    title: 'Governance and Community Guidelines',
+    link: 'https://quantumone.network',
+    description: 'Governance and Community guidelines, dispute resolution, DAO structure, and more.'
   }
 ];
 
