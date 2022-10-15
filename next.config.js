a73a19a66c24be01c526d5c72100c723ccfb62d5
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-15 15:05:44 -0400

    fix next-config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 // next.config.js
 module.exports = {
-  assetPrefix: 'https://example.com',
-  dynamicAssetPrefix: true,
-  inlineImageLimit: 16384,
   poweredByHeader: false,
   webpack(config, options) {
     config.module.rules.push({
