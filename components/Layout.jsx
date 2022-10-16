41da2b88b518218dcf79234c8f4c4f796d4023d4
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-16 01:34:48 -0400

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,8 +13,7 @@ const Layout = ({ children }) => (
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
       <title>QuantaVerse ID</title>
     </Head>
-    <main id="app" className="d-flex flex-column h-100" data-testid="layout" style={{background: "linear-gradient(to right top, #1e2022, #2d424f, #316a7c, #2794a8, #06c1ce, #04cede, #02dcee, #00e9ff, #00d7ff, #00c3ff, #00aeff, #0096ff)", height: '100vh'}}>
-      <NavBar />
+    <main id="app" className="d-flex flex-column h-100" data-testid="layout" style={{background: "linear-gradient(to right top, #1e2022, #2d424f, #316a7c, #2794a8, #06c1ce, #04cede, #02dcee, #00e9ff, #00d7ff, #00c3ff, #00aeff, #0096ff)", height: '100vh'}}> 
       <NavBar />
       <Container className="flex-grow-1 mt-5">{children}</Container>
       <Footer />
