5f5dee9b4f747146a6ad2f1945f02c10754fcb05
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-15 22:51:09 -0400

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,14 +10,11 @@ const Layout = ({ children }) => (
     <Head>
       <link
         rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossOrigin="anonymous"
-      />
-      <link rel="stylesheet" href="https://cdn.auth0.com/js/auth0-samples-theme/1.0/css/auth0-theme.min.css" />
-      <title>Next.js Sample App</title>
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"/>
+      <title>QuantaVerse ID</title>
     </Head>
-    <main id="app" className="d-flex flex-column h-100" data-testid="layout">
+    <main id="app" className="d-flex flex-column h-100" data-testid="layout" style={{background: "linear-gradient(to right top, #1e2022, #2d424f, #316a7c, #2794a8, #06c1ce, #04cede, #02dcee, #00e9ff, #00d7ff, #00c3ff, #00aeff, #0096ff)", height: '100vh'}}>
+      <NavBar />
       <NavBar />
       <Container className="flex-grow-1 mt-5">{children}</Container>
       <Footer />
