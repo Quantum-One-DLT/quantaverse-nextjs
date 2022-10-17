06bab4881176ac6c2420c30fae5092f45f9a2ea5
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-17 14:16:57 -0400

    Update terms.jsx

--- pages/terms.jsx
+++ pages/terms.jsx
@@ -1,1 +1,24 @@
-// future terms page
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+export default withPageAuthRequired(function TermsPage() {
+  return (
+    <>
+      <div className="mb-5" data-testid="terms">
+        <h1 data-testid="terms-title">Quanataverse ID terms of use</h1>
+        <div data-testid="terms-text">
+          <p>
+            This page is in development and will be available ASAP. 
+          </p>
+          <p>
+            In the meantime, please reach out to support@quantaverseid.io with any inquiries you may have. 
+          </p>
+          <p>
+            Quantaverse ID was developed and implemented by Quantum One DAO LLC. 
+          </p>
+        </div>
+      </div>
+    </>
+  );
+});
