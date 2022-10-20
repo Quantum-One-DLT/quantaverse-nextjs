7b459f8162c05c59718485af459d462d9c572e07
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-20 11:34:16 -0400

    Custom callback handler test

--- pages/api/auth/[...auth0].jsx
+++ pages/api/auth/[...auth0].jsx
@@ -1,3 +1,13 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+
+async callback(req, res) {
+    try {
+      await auth0.handleCallback(req, res)
+    } catch (error) {
+      res.redirect('/')
+    }
+  }
+
+});
