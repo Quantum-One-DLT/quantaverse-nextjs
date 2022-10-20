d9a9998a2a55357499dcf4a30b5e05b44cc14d4c
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-20 12:29:26 -0400

    Update [...auth0].jsx

--- pages/api/auth/[...auth0].jsx
+++ pages/api/auth/[...auth0].jsx
@@ -1,13 +1,3 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-
-async callback(req, res) {
-    try {
-      await auth0.handleCallback(req, res)
-    } catch (error) {
-      res.redirect('/')
-    }
-  }
-
-});
+export default handleAuth();
